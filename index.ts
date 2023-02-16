265916dc1e311c849ca48c3b5dda7044a79fcbd5
KristhDev <kristhdev@gmail.com>
2023-02-16 11:34:37 -0600

    Test: current date

--- index.ts
+++ index.ts
@@ -14,7 +14,7 @@ import { coursesNotification, reportNotification, revisitsNotification } from '.
 dotenv.config();
 
 /* A cron job that runs every day at 6:00 AM. */
-schedule('33 11 * * *', () => {
+schedule('36 11 * * *', () => {
     reportNotification();
     revisitsNotification();
     coursesNotification();
