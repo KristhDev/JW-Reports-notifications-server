8f7c8c230dac47d9876e09eb22e23725901dd771
KristhDev <kristhdev@gmail.com>
2023-11-08 19:58:47 -0600

    Feat: add endpoint to send new app version notification

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,9 +1,10 @@
 import axios from 'axios';
 
 export interface NotificationOptions {
-    headings: string;
     contents: string;
     external_user_ids: string[];
+    headings: string;
+    launchUrl?: string;
 }
 
 /**
@@ -12,18 +13,22 @@ export interface NotificationOptions {
  * @param {NotificationOptions} options - The options for the notification.
  * @return {Promise<void>} A promise that resolves when the notification is sent.
  */
-export const sendNotification = async ({ contents, external_user_ids, headings }: NotificationOptions): Promise<void> => {
+export const sendNotification = async ({ contents, external_user_ids, headings, launchUrl }: NotificationOptions): Promise<void> => {
     try {
-        await axios.post('https://onesignal.com/api/v1/notifications', 
-            {
-                app_id: process.env.ONESIGNAL_APP_ID!,
-                contents: { en: contents },
-                headings: { en: headings },
-                include_aliases: {
-                    external_id: external_user_ids
-                },
-                target_channel: 'push',
+        let body = {
+            app_id: process.env.ONESIGNAL_APP_ID!,
+            contents: { en: contents },
+            headings: { en: headings },
+            include_aliases: {
+                external_id: external_user_ids
             },
+            target_channel: 'push',
+        }
+
+        if (launchUrl) body = Object.assign(body, { url: launchUrl });
+
+        await axios.post('https://onesignal.com/api/v1/notifications', 
+            body,
             {
                 headers: {
                 Authorization: `Basic ${ process.env.ONESIGNAL_REST_API_KEY }`,
