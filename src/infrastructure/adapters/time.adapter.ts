b0da8d5aa99383a6376d41b4c3eb972ea4637262
KristhDev <kristhdev@gmail.com>
2025-08-05 08:09:44 -0600

    Feat(adapters): make timezone configurable in TimeAdapter

--- src/infrastructure/adapters/time.adapter.ts
+++ src/infrastructure/adapters/time.adapter.ts
@@ -5,10 +5,31 @@ import utc from 'dayjs/plugin/utc';
 /* Contracts */
 import { TimeAdapterContract } from '@domain/contracts/adapters';
 
+/* Interfaces */
+import { TimeAdapterOptions } from '@infrastructure/interfaces';
+
 export class TimeAdapter implements TimeAdapterContract {
-    constructor() {
+    private readonly options: TimeAdapterOptions;
+    private readonly defaultOptions: TimeAdapterOptions = {
+        timezone: undefined
+    }
+
+    constructor(options?: TimeAdapterOptions) {
+        this.options = { ...this.defaultOptions, ...options };
+
         dayjs.extend(timezone);
         dayjs.extend(utc);
+    }
+
+    /**
+     * Returns the formatted date string.
+     * 
+     * @param {Date | string | number} date - The date to be formatted.
+     * @param {string} format - The format to use for the date string.
+     * @return {string} The formatted date string.
+     */
+    public format(date: Date | string | number, format: string): string {
+        return dayjs(date).tz(this.options.timezone).format(format);
     }
 
     /**
@@ -17,7 +38,7 @@ export class TimeAdapter implements TimeAdapterContract {
      * @return {number} The current day of the month (1-31) as a number.
      */
     public getCurrentDay(): number {
-        return dayjs().tz('America/Managua').get('D');
+        return dayjs().tz(this.options.timezone).get('D');
     }
 
     /**
@@ -26,7 +47,7 @@ export class TimeAdapter implements TimeAdapterContract {
      * @return {number} The last day of the current month as a number.
      */
     public getLastDayOfCurrentMonth(): number {
-        return dayjs().tz('America/Managua').endOf('month').get('D');
+        return dayjs().tz(this.options.timezone).endOf('month').get('D');
     }
 
     /**
@@ -36,6 +57,6 @@ export class TimeAdapter implements TimeAdapterContract {
      * @return {string} The current date and time as a string in the specified format.
      */
     public nowWithFormat(format: string): string {
-        return dayjs().tz('America/Managua').format(format);
+        return dayjs().tz(this.options.timezone).format(format);
     }
 }
