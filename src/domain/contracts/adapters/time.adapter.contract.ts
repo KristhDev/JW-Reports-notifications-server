c2430a83ab9963d7789c5c9bacdcc47aba1cc932
KristhDev <kristhdev@gmail.com>
2025-08-05 08:09:25 -0600

    Feat(contracts): add format method to TimeAdapterContract

--- src/domain/contracts/adapters/time.adapter.contract.ts
+++ src/domain/contracts/adapters/time.adapter.contract.ts
@@ -1,4 +1,5 @@
 export abstract class TimeAdapterContract {
+    public abstract format(date: Date | string | number, format: string): string;
     public abstract getCurrentDay(): number;
     public abstract getLastDayOfCurrentMonth(): number;
     public abstract nowWithFormat(format: string): string;
